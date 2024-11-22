8178332d17f2215eec087014e429de86f06e17b9
Joey <39647@ma-web.nl>
2024-11-22 12:42:52 +0100

    03

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Functions
+﻿using System.Runtime.InteropServices;
+
+namespace Functions
 {
     internal class Program
     {
@@ -9,8 +11,43 @@
         }
         internal void Run() 
         {
-            Console.WriteLine("Dit is nu de start van mijn programma");
+            Console.WriteLine(vraag1, vraag2, vraag3, vraag4, vraag5);
         }
 
+        internal void vraag1()
+        {
+            Console.WriteLine("How long do you think you´d survive in a zombie apocalypse?" );
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void vraag2()
+        {
+            Console.WriteLine("What would you do if you won a million dollars?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void vraag3()
+        {
+            Console.WriteLine("Who is your favorite cs player");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void vraag4()
+        {
+            Console.WriteLine("smash or pass? Yae Miko.");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+        internal void vraag5()
+        {
+            Console.WriteLine("Those who know?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
     }
 }
