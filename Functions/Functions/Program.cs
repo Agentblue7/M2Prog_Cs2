02ca853a2004558fdba4f24f883d2165a007570b
Joey <39647@ma-web.nl>
2024-11-26 10:06:44 +0100

    MAngo
    mango

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -1,9 +1,28 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 
 namespace Functions
 {
     internal class Program
     {
+        string[] vragen = new string[]
+        {
+            "What 1997 N64 video game features James Bond and is named after the 1995 film?",
+            "What arcade game was called puckman in Japan"
+        };
+
+        internal string GetVraag(int vraagIndex)
+        {
+            return vragen[vraagIndex];
+        }
+
+        internal string GetRandomVraag()
+        {
+            //gebruik random 
+            int random = ...;
+            return...;
+        }
         static void Main(string[] args)
         {
             Program program = new Program(); //we hebben deze gemaakt en het is een progrramma type
@@ -11,7 +30,12 @@ namespace Functions
         }
         internal void Run() 
         {
-            Console.WriteLine(vraag1, vraag2, vraag3, vraag4, vraag5);
+            Console.WriteLine(vraag1())
+            Console.WriteLine(vraag2());
+            Console.WriteLine(vraag3());
+            Console.WriteLine(vraag4());
+            Console.WriteLine(vraag5());   
+            string vraag0 = GetVraag(0);
         }
 
         internal void vraag1()
@@ -48,6 +72,16 @@ namespace Functions
             string antwoord = Console.ReadLine();
 
             Console.WriteLine(antwoord);
+        }
+        internal string vraag6()
+        {
+            Console.WriteLine("How long is your cock");
+                string antwoord = Console.ReadLine();
+
+            return antwoord;
+
+            string antwoord6 = Vraag6();
+            Console.WriteLine(antwoord6);
         }
     }
 }
